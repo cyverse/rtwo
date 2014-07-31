6e51ad409ca0c71d7aef051d8f98ef4680927de1
J. Matt Peterson <jmatt@jmatt.org>
2014-07-30 20:35:23 -0700

    Fix and refactor occupancy. 	modified:   rtwo/meta.py

--- rtwo/meta.py
+++ rtwo/meta.py
@@ -5,6 +5,7 @@ Atmosphere service meta.
 import sys
 from abc import ABCMeta
 from math import floor
+import urlparse
 
 from threepio import logger
 
@@ -235,7 +236,111 @@ class OSMeta(Meta):
                 remove_totals['disk'] = remove_totals['disk'] + oc_size.disk
         return remove_totals
 
-    def occupancy(self, overcommited=False):
+
+    def _instance_capacity_on_node(self, size, node):
+        pass
+
+    def _instance_capacity_vcpus(self):
+        pass
+
+    def _sum_active_compute_nodes(self):
+        acs = self._active_compute_nodes()
+        return {"total_vcpus": sum([ac["vcpus"] for ac in acs]),
+                "total_memory_mb": sum([ac["memory_mb"] for ac in acs]),
+                "total_local_gb": sum(ac["local_gb"] for ac in acs)}
+
+    def _active_compute_nodes(self):
+        active_nodes = set(
+            [self._scrub_hostname(n["host"])\
+             for n in self.admin_driver._connection.ex_os_services()\
+             if n["status"] == "enabled"])
+        nodes = {}
+        for n in self.admin_driver._connection.ex_detail_hypervisor_nodes():
+            hostname = self._scrub_hostname(n["hypervisor_hostname"])
+            if hostname in active_nodes:
+                nodes[hostname] = n
+        return nodes
+
+    def _get_node(self, nodes, instance):
+        hostname = instance\
+            .extra["object"]\
+            .get("OS-EXT-SRV-ATTR:hypervisor_hostname")
+        if hostname:
+            hostname = self._scrub_hostname(hostname)
+        return nodes.get(hostname)
+
+    def _get_size(self, sizes, instance):
+        size_id = instance.size.id
+        size = [s for s in sizes if s.id == size_id]
+        if size and len(size) == 1:
+            return size[0]
+        else:
+            return None
+
+    def _scrub_hostname(self, hostname):
+        if not hostname:
+            return None
+        url = urlparse.urlparse(hostname)
+        if not url.hostname:
+            return url.path.split(".")[0]
+        else:
+            return url.hostname.split(".")[0]
+
+    def _get_hashable_node(self, node):
+        if not node:
+            return None
+        return self._scrub_hostname(node["hypervisor_hostname"])
+
+    def _add_occupancy(self, occupancy, node, size, instance):
+        node_key = self._get_hashable_node(node)
+        occ = occupancy.get(node_key)
+        if not occ:
+            occupancy[node_key] = {
+                "cpu": 0,
+                "mem": 0,
+                "disk": 0,
+                "instances": []
+            }
+            occ = occupancy[node_key]
+        occ["cpu"] += size.cpu
+        occ["mem"] += size.ram
+        occ["disk"] += size.ephemeral
+        occ["instances"].append(instance)
+
+# for host, stats in rahr.items():
+#         if not host:
+#             continue
+#         node = nodes[host]
+#         max_cpu = node["vcpus"]
+#         max_mem = node["memory_mb"]
+#         print("host[{0}] cpu ratio({1}/{2}vcpus): {3:03.5f} memory ratio ({4}/{5}MB): {6:03.5f}".format(
+#             host,
+#             stats["cpu"],
+#             max_cpu,
+#             stats["cpu"]/(1.0*max_cpu),
+#             stats["mem"],
+#             max_mem,
+#             stats["mem"]/(1.0*max_mem)))
+
+    def occupancy(self, overcommited=True):
+        """
+        Calculate occupancy using an admin account.
+
+        Get size, instance and compute node data then
+        calculate a better occupancy than _ex_hypervisor_statistics.
+        Our statistics only look at instance states that use resources
+        on the compute node.
+        """
+        sizes = self.admin_driver.list_sizes()
+        nodes = self._active_compute_nodes()
+        occupancy = {}
+        for i in self.all_instances():
+            node = self._get_node(nodes, i)
+            size = self._get_size(sizes, i)
+            self._add_occupancy(occupancy, node, size, i)
+        return occupancy
+
+    def old_occupancy(self, overcommited=False):
         """
         Add Occupancy data to NodeSize.extra
         """
