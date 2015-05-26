3609012b4b5d254e8ca8c7eae34ed6bee5b01359
J. Matt Peterson <jmatt@jmatt.org>
2015-05-25 21:31:04 -0700

    [ATMO-826] swap_service_endpoint bug.
    	   * url could be either an attribute or dictionary key.
    	   * Example of problem ex_update_volume_metadata method.
    
    	modified:   rtwo/drivers/openstack.py
    	modified:   rtwo/version.py

--- rtwo/drivers/openstack.py
+++ rtwo/drivers/openstack.py
@@ -62,7 +62,10 @@ def swap_service_catalog(service_type=None, name=None):
                 new_service = lc_conn.service_catalog.get_endpoint(
                     service_type=service_type, name=name,
                     region=lc_conn.service_region)
-                lc_conn._ex_force_base_url = new_service["url"]
+                if getattr(new_service, "url"):
+                    lc_conn._ex_force_base_url = new_service.url
+                else:
+                    lc_conn._ex_force_base_url = new_service["url"]
                 return method(*args, **kwargs)
             finally:
                 lc_conn._ex_force_base_url = old_endpoint
