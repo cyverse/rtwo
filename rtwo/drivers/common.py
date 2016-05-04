36638e731b6a92d9df02e4fdecae865bbd3ebf2f
Steve Gregory <contact@steve-gregory.com>
2016-05-04 10:48:05 -0700

    Version++

--- rtwo/drivers/common.py
+++ rtwo/drivers/common.py
@@ -231,7 +231,6 @@ def get_ranges(uid_number, inc=0):
     return (block1, block2)
 
 
-
 def get_default_subnet(username, inc=0, get_uid_number=None):
     """
     Return the default subnet for the username and provider.
