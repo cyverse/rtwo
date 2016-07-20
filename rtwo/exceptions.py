cf727ece088bfbaf0b104f13f71a5dd3bcb48710
Steve Gregory <contact@steve-gregory.com>
2016-07-20 10:31:28 -0700

    Internalize neutronclient, libcloud, and glance exceptions
    avoid leaking clients exceptions into Atmosphere
    
    	modified:   rtwo/exceptions.py

--- rtwo/exceptions.py
+++ rtwo/exceptions.py
@@ -2,8 +2,11 @@
 Atmosphere service exceptions.
 
 """
-from neutronclient.common.exceptions import NeutronClientException, NotFound as NeutronNotFound, BadRequest as NeutronBadRequest
+from neutronclient.common.exceptions import NeutronClientException, NotFound as NeutronNotFound, BadRequest as NeutronBadRequest, OverLimit as NeutronOverLimit, NeutronException
 from keystoneclient.apiclient.exceptions import Unauthorized as KeystoneUnauthorized
+from glanceclient.exc import HTTPConflict as GlanceConflict, HTTPForbidden as GlanceForbidden
+from libcloud.common.types import InvalidCredsError as LibcloudInvalidCredsError, MalformedResponseError as LibcloudBadResponseError
+from libcloud.compute.types import DeploymentError as LibcloudDeploymentError
 
 
 class NonZeroDeploymentException(Exception):
