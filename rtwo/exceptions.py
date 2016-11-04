dd2670528710a43aa796e9d8604d8b9741ce9c81
Steve Gregory <contact@steve-gregory.com>
2016-11-04 16:59:48 -0700

    Fix import after update of requirements.txt

--- rtwo/exceptions.py
+++ rtwo/exceptions.py
@@ -4,7 +4,7 @@ Atmosphere service exceptions.
 """
 from neutronclient.common.exceptions import NeutronClientException, NotFound as NeutronNotFound, BadRequest as NeutronBadRequest, NeutronException
 from novaclient.exceptions import OverLimit as NovaOverLimit
-from keystoneclient.apiclient.exceptions import Unauthorized as KeystoneUnauthorized
+from keystoneclient.exceptions import Unauthorized as KeystoneUnauthorized
 from glanceclient.exc import HTTPConflict as GlanceConflict, HTTPForbidden as GlanceForbidden, ClientException as GlanceClientException
 from libcloud.common.types import InvalidCredsError as LibcloudInvalidCredsError, MalformedResponseError as LibcloudBadResponseError
 from libcloud.compute.types import DeploymentError as LibcloudDeploymentError
