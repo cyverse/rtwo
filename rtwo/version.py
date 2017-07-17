36120c97b15bc3eec950e654779fd5a80e32d19a
Steve Gregory <contact@steve-gregory.com>
2017-07-17 08:09:25 -0700

    v0.5.11 - Support for sahara + fix for 'invalid project names when using underscore'

--- rtwo/version.py
+++ rtwo/version.py
@@ -5,7 +5,7 @@ Current logger version.
 from subprocess import Popen, PIPE
 from os.path import abspath, dirname
 
-VERSION = (0, 5, 10, 'dev', 0)
+VERSION = (0, 5, 11, 'dev', 0)
 
 
 def version_str():
