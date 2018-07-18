8e4ac468af2282246c5adfc6dfcd9d20bacdcee9
Connor Osborn <cdosborn@email.arizona.edu>
2018-07-18 15:14:09 -0700

    WIP

--- rtwo/__init__.py
+++ rtwo/__init__.py
@@ -1,7 +1,6 @@
-""" rtwo
-
-"""
-#FIXME: Remove next 3 lines?
-import libcloud.security
-libcloud.security.VERIFY_SSL_CERT = False
-libcloud.security.VERIFY_SSL_CERT_STRICT = False
+try:
+    import libcloud.security
+    libcloud.security.VERIFY_SSL_CERT = False
+    libcloud.security.VERIFY_SSL_CERT_STRICT = False
+except ImportError:
+    pass
